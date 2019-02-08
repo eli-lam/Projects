9b4e28c026452b24bc901f2a2e07724547b04160
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2019-02-08 15:22:15 -0500

    Created C++ folder

--- C++/FizzBuzz.cpp
+++ C++/FizzBuzz.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main(){
     int count = 1;
 
     for(count; count<101; count++){
         if (count % 3 == 0 && count % 5 == 0)
-            std::cout << "FizzBuzz ";
+            cout << "FizzBuzz ";
         else if (count % 3 == 0)
-            std::cout << "Fizz ";
+            cout << "Fizz ";
         else if (count % 5 == 0)
-            std::cout << "Buzz ";
+            cout << "Buzz ";
         else
-            std::cout << count << " ";
+            cout << count << " ";
     }
 }
